dfa716e95fabb8cb239502c861d0d005f0688325
andredochute <andredochute@gmail.com>
2022-08-11 11:52:36 -0400

    js da converção de moedas

---
+++ moeda.js
@@ -0,0 +1,22 @@
+
+
+
+function calcular(){
+
+    const dolar = document.getElementById("dolar");
+    const euro = document.getElementById("euro");
+    const valor = document.getElementById("valor");
+
+    var select = document.getElementById('moeda');
+    var option = select.options[select.selectedIndex].value;
+
+    if(option == "dolar"){
+
+        var soma = valor.value*5.15
+        resultado.textContent = soma
+
+    }
+
+    
+
+};
